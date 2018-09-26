9b69f2afdb35e46d63eb23771bf8ea281a804c37
David T. Pocock <david.pocock.2013@uni.strath.ac.uk>
2018-09-26 14:15:47 +0100

    Add Hamming distance-based fitness function

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import random
 import string
 
-def genRandString():
-    charList = []
-    for chr in range(0,12):
-        chr = random.choice(string.printable)
-        charList.append(chr)
-    str = ''.join(charList)
-    return str
+targetString = "Hello World!"
+populationSize = 1000000
 
-print (genRandString())
+def generatePopulation(populationSize):
+    population = []
+    for i in range(0,populationSize):
+        chromosome = []
+        for char in range(0,12):
+            char = random.choice(string.printable)
+            chromosome.append(char)
+        chromoString = ''.join(chromosome)
+        population.append(chromoString)
+    return population
+
+def fitness(string, targetStr):
+    if len(string) == len(targetStr):
+        value = sum(el1 != el2 for el1, el2 in zip(string, targetStr))
+        return value
+
+"""def checkSimilarity(population targetString):
+    targetString = [Hello World!]
+    for chr in population:
+
+"""
+
+population = generatePopulation(populationSize)
+for str in population:
+    value = fitness(str, targetString)
+    if value < 9:
+        print (value, str)
