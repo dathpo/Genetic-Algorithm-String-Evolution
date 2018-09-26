198f7f2070aa6ea7a74d7e183e76003563ffa5ea
David T. Pocock <david.pocock.2013@uni.strath.ac.uk>
2018-09-26 20:17:46 +0100

    Progress with bit arrays

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import random, string, time
+from bitarray import bitarray
 
-targetString = "Hello World!"
-populationSize = 1000000
+def targetString():
+    return "Hello World!"
+
+def populationSize():
+    return 1000000
 
 def main():
     start = time.time()
-    population = generatePopulation(populationSize)
+    population = generatePopulation(populationSize())
     for str in population:
-        value = fitness(str, targetString)
+        value = fitness(str, targetString())
         if value < 9:
             print (value, str)
     end = time.time()
+    print(crossover(0))
     print("{}{}{}".format("The process took ", end - start, " seconds"))
 
 def generatePopulation(size):
     population = []
     for i in range(0,size):
         chromosome = []
-        for char in range(0,len(targetString)):
+        for char in range(0,len(targetString())):
             char = random.choice(string.printable)
             chromosome.append(char)
         chromoString = ''.join(chromosome)
@@ -32,6 +37,10 @@ def fitness(source, target):
             if a != b:
                 hammingDistance += 1
         return hammingDistance
+
+def crossover(chromosome):
+    bitArray = bitarray(2**2)
+    return bitArray
 
 """def checkSimilarity(population targetString):
     targetString = [Hello World!]
