00d5ebc0ae2f3a5585bd2b96aeab919b5f104976
David T. Pocock <david.pocock.2013@uni.strath.ac.uk>
2018-09-27 21:02:35 +0100

    Add tournament selection process

--- main.py
+++ main.py
@@ -1,51 +1,92 @@
-import random, string, timeit
+import random, string, timeit, operator
 # from bitarray import bitarray
 
-def targetString():
+
+def target_string():
     return "Hello World!"
 
-def populationSize():
-    return 1000000
+
+def population_size():
+    return 1000
+
+
+def available_chars():
+    characters = string.ascii_letters + string.digits + ' ' + string.punctuation
+    return characters
+
+
+def tournament_size():
+    return 2
+
+
+def tournament_rounds():
+    return 5
+
 
 def main():
     start_time = timeit.default_timer()
-    population = generatePopulation(populationSize())
+    population = generate_population(population_size())
     print("Hamming Distance     Chromosome")
     for str in population:
-        value = fitness(str, targetString())
+        value = fitness(str, target_string())
         if value < 9:
-            print("       {}            {}".format(value, repr(str)[1:-1]))
+            print("       {}            {}".format(value, str))
+    print(selection(population))
     # print(crossover(0))
     print("\nThe process took {} seconds".format(timeit.default_timer() - start_time))
 
-def generatePopulation(size):
+
+def generate_population(size):
     population = []
     for i in range(0,size):
         chromosome = []
-        for char in range(0,len(targetString())):
-            char = random.choice(string.printable)
+        str_length = len(target_string())
+        for char in range(0,str_length):
+            if str_length != 12:
+                print("Length: {}", str_length)
+            char = random.choice(available_chars())
             chromosome.append(char)
-        chromoString = ''.join(chromosome)
-        population.append(chromoString)
+        chromo_string = ''.join(chromosome)
+        population.append(chromo_string)
     return population
+
 
 def fitness(source, target):
     if len(source) == len(target):
         pairs = zip(source, target)
-        hammingDistance = 0
+        hamming_distance = 0
         for a, b in pairs:
             if a != b:
-                hammingDistance += 1
-        return hammingDistance
+                hamming_distance += 1
+        return hamming_distance
+
+
+def selection(population):
+    return tournament_selection(population)
+
+
+def tournament_selection(population):
+    for rounds in range(0, tournament_rounds()):
+        candidates = []
+        for candidate in range(0, tournament_size()):
+            random_index = random.randrange(len(population) - 1)
+            candidate = population[random_index]
+            cand_fitness = fitness(candidate, target_string())
+            candidates.append(tuple(candidate, cand_fitness))
+            print(candidate, random_index)
+        print(candidates)
+        winner = max(candidates, key=itemgetter(1))
+        print(winner)
+
+
+def decision(probability):
+    return random.random() < probability
+
 
 # def crossover(chromosome):
-#     bitArray = bitarray(2**2)
-#     return bitArray
+#     bit_array = bitarray(2**2)
+#     return bit_array
 
-"""def checkSimilarity(population targetString):
-    targetString = [Hello World!]
-    for chr in population:
-"""
 
 if __name__ == "__main__":
     main()
