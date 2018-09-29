2d13717e68aa7b8bc6ee9f990cf0171852b5826c
David T. Pocock <davidtpocock@gmail.com>
2018-09-29 22:28:55 +0100

    Add class structure, create main and start hill climbing

---
+++ hill_climbing.py
@@ -0,0 +1,10 @@
+__author__ = 'David T. Pocock'
+
+
+class HillClimbing:
+    def __init__(self, target_string):
+        self.target_string = target_string
+
+
+    def get_target_string(self):
+        return print(self.target_string)
